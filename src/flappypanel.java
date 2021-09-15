8b4cbbc819a7a4dc72c91b500411b6113afe1656
Anudeep Gunukula <anudeepgunukula@gmail.com>
2021-09-15 07:33:01 +0000

    flappy bird in java day 2

--- src/flappypanel.java
+++ src/flappypanel.java
@@ -10,23 +10,40 @@ import java.awt.event.KeyListener;
 
 public class flappypanel extends JPanel implements KeyListener,ActionListener{
 	
+	
+	final int WIDTH=525,HEIGHT=550;
+	int flappyheight=HEIGHT/4;
+	int velcoity=0;
+	int flappyV=0,flappyA=2;
+	
 	public flappypanel()
 	{
-		setSize(525,550);
+		setSize(WIDTH,HEIGHT);
 		setFocusable(true);
 		addKeyListener(this);
 		setBackground(Color.BLACK);
+		new Timer(40,this).start();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		flappyV += flappyA;
+				
+		repaint();
 		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
+		
+		int code=e.getKeyCode();
+		if(code==e.VK_SPACE)
+		{
+			if(flappyV-30 >= -133)
+			        flappyV-=30;
+		}
 		
 	}
 
@@ -40,6 +57,20 @@ public class flappypanel extends JPanel implements KeyListener,ActionListener{
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		drawFlappy(g);
+	}
+	
+	private void drawFlappy(Graphics g)
+	{
+		g.setColor(Color.WHITE);
+		
+		g.fillRect(150, flappyheight+flappyV, 25, 25);
 	}
 
 }
